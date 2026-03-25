3f9a8dfca4db1fbb03d8772ca18327eeee5b1366
Ana Julia Grzyb <anajgrzyb@gmail.com>
2026-03-25 10:20:28 -0300

    Protege os endpoints PUT e DELETE com autenticação

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
@@ -8,7 +10,7 @@ def test_root_deve_retornar_ok_e_ola_mundo(client):
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
-def test_create_user(client):
+def test_create_user(client, token):
     response = client.post(
         '/users/',
         json={
@@ -21,27 +23,24 @@ def test_create_user(client):
     assert response.json() == {
         'username': 'ana',
         'email': 'ana@example.com',
-        'id': 1,
+        'id': 2,
     }
 
 
-def test_read_users(client):
-    response = client.get('/users/')
+def test_read_users_with_users(client, user, token):
+
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get(
+        '/users/', headers={'Authorization': f'Bearer {token}'}
+    )
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'ana',
-                'email': 'ana@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': [user_schema]}
 
 
-def test_update_user(client):
+def test_update_user(client, user, token):
     response = client.put(
         '/users/1',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'ana',
             'email': 'ana@example.com',
@@ -55,3 +54,26 @@ def test_update_user(client):
         'username': 'ana',
         'email': 'ana@example.com',
     }
+
+
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_get_token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+
+    token = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert token['token_type'] == 'Bearer'
+    assert 'access_token' in token
